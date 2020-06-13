70dd747054febb69b4fc31b42dc7aa9d561d7284
Ross A. Baker <ross@rossabaker.com>
2020-06-13 13:27:12 -0400

    Showing my work

---
+++ README.md
@@ -0,0 +1,20 @@
+# Make your contributor graph say Abolish ICE
+
+![Contributor Graph](graph.png)
+
+## Usage
+
+1. Create a new repo.  I like `abolish-ice`
+1. Run it.
+
+```sh
+./paint.sh
+```
+
+### Speed vs. clarity
+
+The script needs to generate many more commits than your busiest days to be legible. your other contributions to make a loud and clear statement.  The `$I` variable determines the number of commits.  The larger the number, the slower it runs.  The smaller the number, the more dark green you may see.
+
+## Credit
+
+Bootstrapped from [github-graffiti](https://github.com/mavrk/github-graffiti).
---
+++ paint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash		
+while read line	 
+do		
+	IFS='/' read -ra PARAMS <<< "$line"
+	D=${PARAMS[0]}
+	M=${PARAMS[1]}
+	Y=${PARAMS[2]}
+	I=180
+	if [ ! -d "$Y" ]; then
+    	mkdir $Y
+	fi	
+		cd $Y
+		if [ ! -d "$M" ]; then
+			mkdir $M
+		fi
+			cd $M
+			if [ ! -d "$D" ]; then
+				mkdir $D
+			fi
+				cd $D
+				for i in $( eval echo {1..$I} )
+      			do
+      				echo "$i on $D/$M/$Y" > commit.md
+                                s=$(printf "%02d" $(expr $i % 60))
+                                m=$(printf "%02d" $(expr $i / 60))
+        			export GIT_COMMITTER_DATE="$Y-$M-$D 12:$m:$s"
+        			export GIT_AUTHOR_DATE="$Y-$M-$D 12:$m:$s"
+                                echo $s $m $GIT_COMMITTER_DATE $GIT_AUTHOR_DATE
+        			git add commit.md -f
+        			git commit --date="$Y-$M-$D 12:$m:$s" -m "$i on $M $D $Y"
+        		done
+        	cd ../
+        cd ../
+    cd ../	
+done < dates.txt
+git push origin master
+git rm -rf 20**
+git commit -am "cleanup"
+git push origin master
