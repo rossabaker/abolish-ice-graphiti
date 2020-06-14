2c92f707a13177403c6886cae8579da747a00926
Ross A. Baker <ross@rossabaker.com>
2020-06-14 14:07:52 -0400

    Optimize with empty commits (fixes #6)

--- paint.sh
+++ paint.sh
@@ -7,23 +7,13 @@ do
 	Y=${PARAMS[2]}
 	I=180
 	d="$Y-$M-$D"
-	if [ ! -d "$d" ]; then
-	    mkdir -p $d
-	fi	
-	pushd $d
 	for i in $( eval echo {1..$I} )
 	do
-		echo "$i on $d" > commit.md
 		s=$(printf "%02d" $(expr $i % 60))
 		m=$(printf "%02d" $(expr $i / 60))
 		export GIT_COMMITTER_DATE="$d 12:$m:$s"
 		export GIT_AUTHOR_DATE="$d 12:$m:$s"
-		git add commit.md -f
-		git commit --date="$d 12:$m:$s" -m "$i on $d" --no-gpg-sign
+		git commit --date="$d 12:$m:$s" -m "$i on $d" --no-gpg-sign --allow-empty
 	done
-        popd
 done < dates.txt
-git push origin master
-git rm -rf 20**
-git commit -am "cleanup" --no-gpg-sign
 git push origin master
